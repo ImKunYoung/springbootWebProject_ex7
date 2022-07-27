936135077295b8d786d304fc8809a193812e79e5
ImKunYoung <lim8667@gmail.com>
2022-07-28 07:19:58 +0900

    @Commit - Transactional 한 쿼리에서 커밋하기 위함

--- src/test/java/ml/mjuchallengers/ex7_mreview/repository/MovieRepositoryTests.java
+++ src/test/java/ml/mjuchallengers/ex7_mreview/repository/MovieRepositoryTests.java
@@ -5,6 +5,7 @@ import ml.mjuchallengers.ex7_mreview.entity.MovieImage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
@@ -19,6 +20,7 @@ public class MovieRepositoryTests {
     @Autowired
     private MovieImageRepository movieImageRepository;
 
+    @Commit
     @Transactional
     @Test
     public void insertMovies() {
