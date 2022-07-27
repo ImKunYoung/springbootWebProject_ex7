f435a23f0d9eaa030e002b6f428cf61446baa116
ImKunYoung <lim8667@gmail.com>
2022-07-28 07:17:12 +0900

    movie, movieimage table 리포지토리 테스트 - 샘플 데이터 삽입

--- src/test/java/ml/mjuchallengers/ex7_mreview/repository/MovieRepositoryTests.java
+++ src/test/java/ml/mjuchallengers/ex7_mreview/repository/MovieRepositoryTests.java
@@ -1,8 +1,14 @@
 package ml.mjuchallengers.ex7_mreview.repository;
 
+import ml.mjuchallengers.ex7_mreview.entity.Movie;
+import ml.mjuchallengers.ex7_mreview.entity.MovieImage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 public class MovieRepositoryTests {
@@ -12,12 +18,36 @@ public class MovieRepositoryTests {
 
     @Autowired
     private MovieImageRepository movieImageRepository;
-    // TODO: -p19
 
+    @Transactional
     @Test
     public void insertMovies() {
 
-        
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+
+            Movie movie = Movie.builder().title("Movie...."+i).build();
+
+            System.out.println("--------------------------------------------");
+
+            movieRepository.save(movie);
+
+            int count = (int)(Math.random() * 5) +1; // 1, 2, 3, 4
+
+            for(int j = 0; j < count; j++) {
+
+                MovieImage movieImage = MovieImage.builder()
+                        .uuid(UUID.randomUUID().toString())
+                        .movie(movie)
+                        .imgName("test"+j+".jpg")
+                        .build();
+
+                movieImageRepository.save(movieImage);
+
+            }
+
+        });
+
+
 
     }
 }
