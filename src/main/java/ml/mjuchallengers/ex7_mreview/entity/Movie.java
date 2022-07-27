f76b285f5bbefeb32d3cc13890ec20d90d6a7caf
ImKunYoung <lim8667@gmail.com>
2022-07-27 17:47:44 +0900

    movie entity 설계

---
+++ src/main/java/ml/mjuchallengers/ex7_mreview/entity/Movie.java
@@ -0,0 +1,21 @@
+package ml.mjuchallengers.ex7_mreview.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    private String title;
+
+}
