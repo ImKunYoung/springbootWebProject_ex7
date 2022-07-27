13e969a19365410ad981af98b51e1483d8f5a943
ImKunYoung <lim8667@gmail.com>
2022-07-27 17:38:18 +0900

    @EnableJpaAuditing - audit: update 발생 시 자동으로 시간 값을 넣ㅇ어줌

--- src/main/java/ml/mjuchallengers/ex7_mreview/Ex7MreviewApplication.java
+++ src/main/java/ml/mjuchallengers/ex7_mreview/Ex7MreviewApplication.java
@@ -2,8 +2,10 @@ package ml.mjuchallengers.ex7_mreview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Ex7MreviewApplication {
 
     public static void main(String[] args) {
