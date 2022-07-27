dca925c663df16a159293c17d90cbadc4049de0b
ImKunYoung <lim8667@gmail.com>
2022-07-27 15:45:50 +0900

    init commit

---
+++ src/main/java/ml/mjuchallengers/ex7_mreview/Ex7MreviewApplication.java
@@ -0,0 +1,13 @@
+package ml.mjuchallengers.ex7_mreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex7MreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex7MreviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ml/mjuchallengers/ex7_mreview/Ex7MreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.mjuchallengers.ex7_mreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex7MreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
