570b3ca47ac4a006e284bd922cc83676feb98ac7
ImKunYoung <lim8667@gmail.com>
2022-07-28 07:03:28 +0900

    movie, movieimage table 리포지토리 테스트 - 샘플 데이터 삽입

--- src/test/java/ml/mjuchallengers/ex7_mreview/repository/MovieRepositoryTests.java
+++ src/test/java/ml/mjuchallengers/ex7_mreview/repository/MovieRepositoryTests.java
@@ -1,5 +1,6 @@
 package ml.mjuchallengers.ex7_mreview.repository;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,4 +13,11 @@ public class MovieRepositoryTests {
     @Autowired
     private MovieImageRepository movieImageRepository;
     // TODO: -p19
+
+    @Test
+    public void insertMovies() {
+
+        
+
+    }
 }
