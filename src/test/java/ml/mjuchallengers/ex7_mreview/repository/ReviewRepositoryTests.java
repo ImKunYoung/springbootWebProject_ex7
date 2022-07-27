d3c87fc19d9d512f845d4765e8eebe504d868a5b
ImKunYoung <lim8667@gmail.com>
2022-07-28 08:04:26 +0900

    Review (매핑) 테이블 더미 데이터 추가

--- src/test/java/ml/mjuchallengers/ex7_mreview/repository/ReviewRepositoryTests.java
+++ src/test/java/ml/mjuchallengers/ex7_mreview/repository/ReviewRepositoryTests.java
@@ -1,14 +1,44 @@
 package ml.mjuchallengers.ex7_mreview.repository;
 
+import ml.mjuchallengers.ex7_mreview.entity.Member;
+import ml.mjuchallengers.ex7_mreview.entity.Movie;
+import ml.mjuchallengers.ex7_mreview.entity.Review;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 public class ReviewRepositoryTests {
 
     @Autowired
-    private ReviewRepository repository;
+    private ReviewRepository reviewRepository;
 
+    @Test
+    public void insertMovieReviews() {
 
+        IntStream.rangeClosed(1, 200).forEach(i -> {
+
+            Long mno = (long)(Math.random() * 100) + 1;
+            Movie movie = Movie.builder().mno(mno).build();
+
+            Long mid = (long)(Math.random() * 100) + 1;
+            Member member = Member.builder().mid(mid).build();
+
+            int grade = (int)(Math.random()*5)+1;
+
+            Review review = Review.builder()
+                    .member(member)
+                    .movie(movie)
+                    .grade(grade)
+                    .text("이 영화에 대한 느낌..."+i)
+                    .build();
+
+            reviewRepository.save(review);
+
+        });
+
+    }
 
 }
