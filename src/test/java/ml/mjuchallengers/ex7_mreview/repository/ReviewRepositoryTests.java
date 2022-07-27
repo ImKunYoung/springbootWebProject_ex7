8994fff23a3163ec6642708b83e456925d5df96f
ImKunYoung <lim8667@gmail.com>
2022-07-28 07:45:53 +0900

    Review 리포지토리 테스트 클래스에 빈 등록

--- src/test/java/ml/mjuchallengers/ex7_mreview/repository/ReviewRepositoryTests.java
+++ src/test/java/ml/mjuchallengers/ex7_mreview/repository/ReviewRepositoryTests.java
@@ -1,9 +1,13 @@
 package ml.mjuchallengers.ex7_mreview.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ReviewRepositoryTests {
+
+    @Autowired
+    private ReviewRepository repository;
 
 
 
