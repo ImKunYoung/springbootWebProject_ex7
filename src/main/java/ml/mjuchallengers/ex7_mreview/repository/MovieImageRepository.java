ce76d578542a4a6d6936a2b110824cb1f69019ba
ImKunYoung <lim8667@gmail.com>
2022-07-28 20:08:04 +0900

    페이지 처리되는 영화별 평균 점수 / 리뷰 개수 구하기 - 리포지터리
    coalesce(r.grade, 0) - r.grade  값이 null 이면 0 리턴

--- src/main/java/ml/mjuchallengers/ex7_mreview/repository/MovieImageRepository.java
+++ src/main/java/ml/mjuchallengers/ex7_mreview/repository/MovieImageRepository.java
@@ -1,15 +1,9 @@
 package ml.mjuchallengers.ex7_mreview.repository;
 
 import ml.mjuchallengers.ex7_mreview.entity.MovieImage;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface MovieImageRepository extends JpaRepository<MovieImage, Long> {
 
-    /*평점과 리뷰 개수 조회 쿼리*/
-    @Query("select m, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m left outer join Review r on r.movie = m group by m")
-    Page<Object[]> getListPage(Pageable pageable);
 
 }
