7184f4aa019924e840889934a1c029fc8d3428e7
ImKunYoung <lim8667@gmail.com>
2022-07-29 10:32:51 +0900

    특정 영화의 모든 이미지 조회 쿼리
    SELECT movie.mno, movie.title, movie_image.inum, movie_image.img_name
    FROM movie
    LEFT OUTER JOIN movie_image ON movie_image.movie_mno = movie.mno
    WHERE movie.mno = '1';

--- src/main/java/ml/mjuchallengers/ex7_mreview/repository/MovieRepository.java
+++ src/main/java/ml/mjuchallengers/ex7_mreview/repository/MovieRepository.java
@@ -6,10 +6,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     /*평점과 리뷰 개수 조회 쿼리*/
     @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m left outer join Review r on r.movie = m left outer join MovieImage mi on mi.movie = m group by m")
     Page<Object[]> getListPage(Pageable pageable);
+
+    /*특정 영화의 모든 이미지와 평균 평점/리뷰 개수*/
+    /*특정 영화의 모든 이미지 조회 쿼리*/
+    @Query("select movie, movieImage from Movie movie left outer join MovieImage movieImage on movieImage.movie = movie where movie.mno = :mno")
+    List<Object[]> getMovieWithAll(Long mno);
 
 }
