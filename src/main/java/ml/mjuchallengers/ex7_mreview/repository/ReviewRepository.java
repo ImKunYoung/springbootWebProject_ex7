57a50d068b622b166e21b8f8768aa35510016244
ImKunYoung <lim8667@gmail.com>
2022-07-29 11:18:01 +0900

    특정 영화의 모든 리뷰 조회 쿼리

--- src/main/java/ml/mjuchallengers/ex7_mreview/repository/ReviewRepository.java
+++ src/main/java/ml/mjuchallengers/ex7_mreview/repository/ReviewRepository.java
@@ -1,7 +1,14 @@
 package ml.mjuchallengers.ex7_mreview.repository;
 
+import ml.mjuchallengers.ex7_mreview.entity.Movie;
 import ml.mjuchallengers.ex7_mreview.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    /*특정 영화의 모든 리뷰 조회 쿼리*/
+    List<Review> findByMovie(Movie movie);
+
 }
